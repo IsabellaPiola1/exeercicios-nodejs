d22eb9291da1dccabd5f3c5cc4be2a1caa3d8814
isabellapiola1 <rm93960@fiap.com.br>
2023-02-27 10:57:03 -0300

    exercicios

---
+++ temperatura.js
@@ -0,0 +1,13 @@
+let temperatura = 13;
+// let frio = temperatura < 15; //false
+
+if (temperatura < 15) {
+    console.log("Está frio");
+} 
+
+if (temperatura > 25) {
+    console.log("Está calor");
+}
+
+// Quando o codigo esta isolado, não esta condicionado a ser executado sobre determinada ação 
+// o IF condiciona trechos do programa
---
+++ temperatura2.js
@@ -0,0 +1,13 @@
+let temperatura = 11;
+console.log("Temperatura: ", temperatura);
+
+if (temperatura < 10) {
+    console.log("Esta frio");
+
+} else {
+    if (temperatura > 25){
+        console.log("Esta calor");
+    }
+}
+
+console.log("Fim do programa");
